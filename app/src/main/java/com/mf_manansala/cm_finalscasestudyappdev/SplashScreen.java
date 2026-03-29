9ea4f27ae6e036113591677734bfb913d36fd55d
MarthaManansala <mf.manansala.bsit2024@gmail.com>
2026-03-30 00:33:39 +0800

    transferred files from previous project

---
+++ app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.mf_manansala.cm_finalscasestudyappdev;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashScreen extends Activity {
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        intent = new Intent(".MainActivity");
+
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(1500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
