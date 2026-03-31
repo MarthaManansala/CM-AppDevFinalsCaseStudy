d6bd1e71c7013767ff821796c166fcafa961fade
MarthaManansala <mf.manansala.bsit2024@gmail.com>
2026-03-31 19:59:46 +0800

    added months' layouts and classes

---
+++ app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/November.java
@@ -0,0 +1,13 @@
+package com.mf_manansala.cm_finalscasestudyappdev;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class November extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.november);
+    }
+}
---
+++ app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/October.java
@@ -0,0 +1,13 @@
+package com.mf_manansala.cm_finalscasestudyappdev;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class October extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.october);
+    }
+}
