26f5f4152b135ee646d101435659d6e2b9791bcc
MarthaManansala <mf.manansala.bsit2024@gmail.com>
2026-03-31 23:22:28 +0800

    Add/View/Edit/Delete Layout

---
+++ app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/AddViewEditDelete.java
@@ -0,0 +1,13 @@
+package com.mf_manansala.cm_finalscasestudyappdev;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddViewEditDelete extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_view_edit_delete);
+    }
+}
