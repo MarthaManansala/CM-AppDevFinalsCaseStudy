32c63e496a161c5bbcd16f0e11667713fcf0c91b
MarthaManansala <mf.manansala.bsit2024@gmail.com>
2026-04-01 18:14:18 +0800

    backend for months + android manifest

--- app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/SetOfButtonsSeptember.java
+++ app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/SetOfButtonsSeptember.java
@@ -3,10 +3,10 @@ package com.mf_manansala.cm_finalscasestudyappdev;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SetOfButtonsMarch extends Activity{
-        @Override
+public class SetOfButtonsSeptember extends Activity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.set_of_buttons_march);
+        setContentView(R.layout.set_of_buttons_september);
     }
 }
