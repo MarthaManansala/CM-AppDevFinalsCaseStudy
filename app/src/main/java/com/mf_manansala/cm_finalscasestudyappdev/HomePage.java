69a192c3eaa6630d303e6f4f1581615d8784e183
MarthaManansala <mf.manansala.bsit2024@gmail.com>
2026-04-15 14:01:06 +0800

    polish

--- app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/HomePage.java
+++ app/src/main/java/com/mf_manansala/cm_finalscasestudyappdev/HomePage.java
@@ -8,11 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Currency;
 import java.util.Locale;
 
 public class HomePage extends Activity {
